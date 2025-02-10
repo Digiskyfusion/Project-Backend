8626db03b297cf2abba73e97ad3d204b5fb4d6e5
Manish6906 <20-m-0742@rayatbahrauniversity.edu.in>
2025-02-10 17:45:39 +0530

    did some changes

--- Model/plan.js
+++ Model/plan.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 const PlanSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },  // Price in USD or any currency
-  duration: { type: Number, required: true },  // Duration in months
-  features: [{ type: String }],  // List of features for this plan
+  credit: { type: Number, required: true },  // Price in USD or any currency
+  amount: { type: Number, required: true },  // Duration in months
   createdAt: { type: Date, default: Date.now },
 });
 
