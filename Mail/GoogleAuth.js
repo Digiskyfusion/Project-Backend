98a26d8cea1b069eddc6bdd4b73148556e9cb6e2
Manish6906 <20-m-0742@rayatbahrauniversity.edu.in>
2025-02-28 19:44:36 +0530

    working on google authentication

---
+++ Mail/GoogleAuth.js
@@ -0,0 +1,43 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../Model/user");
+
+passport.use(
+  new GoogleStrategy(
+  {
+    clientID: process.env.CLIEND_Id,
+    clientSecret:process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+  },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ email: profile.emails[0].value });
+
+        if (!user) {
+          user = new User({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+          });
+          await user.save();
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
