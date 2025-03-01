78272b2aeccb069ef9d26c367195fa1c48f17c62
Manish6906 <20-m-0742@rayatbahrauniversity.edu.in>
2025-03-01 18:11:25 +0530

    forget password

--- Mail/GoogleAuth.js
+++ Mail/GoogleAuth.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../Model/user");
-
+//google
 passport.use(
   new GoogleStrategy(
   {
