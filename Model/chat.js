67cdc8311bc0d84985ceacc1213c22fb3eea69fd
Manish6906 <20-m-0742@rayatbahrauniversity.edu.in>
2025-03-05 10:53:26 +0530

    update

--- Model/chat.js
+++ Model/chat.js
@@ -4,7 +4,6 @@ const chatSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   message: { type: String, required: true },
-  roleType: { type: String, enum: ["freelancer", "client"], required: true },
   createdAt: { type: Date, default: Date.now },
   timestamp: { type: Date, default: Date.now },
 });
