2d2b8c69e9cd606a32c731466b7b7daf1cdd1888
Anil kumar <kumaranilstps017@gmail.com>
2025-03-10 10:54:06 +0530

    fix: bcrypt import

--- Controllers/AuthControllers.js
+++ Controllers/AuthControllers.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const app = express();
-const bcrypt= require("bcryptjs");
+const bcrypt= require("bcrypt");
 const jwt= require("jsonwebtoken")
 const userModel= require("../Model/user");
 const cookieParser = require("cookie-parser");
