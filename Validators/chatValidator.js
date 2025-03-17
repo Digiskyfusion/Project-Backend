d00e398eecd931f388bc007e2427a1b83286b1ae
Digiskyfusion <digiskyfusion@gmail.com>
2025-03-17 09:54:46 +0530

    Delete Validators directory

--- Validators/chatValidator.js
+++
@@ -1,9 +0,0 @@
-// /validators/chatValidator.js
-const { check } = require('express-validator');
-
-const validateChatMessage = [
-    check('receiver', 'Receiver ID is required').notEmpty(),
-    check('message', 'Message cannot be empty').notEmpty(),
-];
-
-module.exports ={validateChatMessage}
