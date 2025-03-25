af14baaf4b4046533c888224ac909e2eb372cadb
Digiskyfusion <digiskyfusion@gmail.com>
2025-03-25 09:38:35 +0530

    Delete src directory

--- src/routes/oAuth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router()
-import { authConsent } from "../shared/googleAuth.js";
-
-router.post('/', async function (req,res){
-        await authConsent(req, res);
-})
-
-export default router;
