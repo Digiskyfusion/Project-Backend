9ec65dff49dffd080223e1588c637a7071b4e5f2
priya <priyasoni6027@gmail.com>
2025-03-26 11:05:06 +0530

    updated index.js

--- src/routes/authenticated/payment.js
+++ src/routes/authenticated/payment.js
@@ -3,9 +3,9 @@ import paymentController from "../../controllers/authenticated/payment.js";
 import verifyToken from "../../middlewares/authentication.js";
 const router = express.Router();
 
-// router.get("/payment/:userId", paymentController.getPaymentDataByUserId);
+router.get("/payment/:userId", paymentController.getPaymentDataByUserId);
 router.post("/order/:packageId", verifyToken, paymentController.createOrder);
 router.post("/verify/:id", paymentController.verifyPayment);
-// router.get("/:status", paymentController.getPaymentsWithStatus);
+router.get("/:status", paymentController.getPaymentsWithStatus);
 
 export default router;
