4099f0fea6d45f9f5e809e2b1b21a66161153c95
priya <priyasoni6027@gmail.com>
2025-03-29 17:21:39 +0530

    Remove routes folder temporarily

--- src/routes_temp/user.js
+++ src/routes_temp/user.js
@@ -7,7 +7,7 @@ import {
   updateUser, 
   deleteUser, 
   getUsersBySkills 
-} from "../controller/user.js"; // Ensure this path is correct
+} from "../controller_temp/user"; // Ensure this path is correct
 
 const router = express.Router();
 
