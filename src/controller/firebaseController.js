55a03e52e65b5aa3c78fb57533035e2586997894
bhavya-bansal-29 <bhavyabansal1357@gmail.com>
2025-05-24 12:04:21 +0530

    notification code commenting

--- src/controller/firebaseController.js
+++ src/controller/firebaseController.js
@@ -3,8 +3,8 @@
 
 export const sendFireBaseNotification = async (req, res) => {
   try {
-    const {title, body, deviceToken} = req.body;
-    await NotificationService.sendNotification(deviceToken, title, body);
+    // const {title, body, deviceToken} = req.body;
+    // await NotificationService.sendNotification(deviceToken, title, body);
     res.status(200).json({ message: "Notification Sent successfully", success: true });
   } catch (error) {
     res.status(500).json({ message: "Error sending notification", success: false });
