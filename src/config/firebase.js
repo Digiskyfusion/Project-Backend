ff4e22ba492bb901ce376c9b983632131ee4d5eb
bhavya-bansal-29 <bhavyabansal1357@gmail.com>
2025-05-29 12:52:41 +0530

    issues

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,19 +1,13 @@
 import admin from 'firebase-admin';
 import { readFileSync } from 'fs';
 import path from 'path';
-const base64Key = process.env.FireBase_Key_BASE64;
 
-if (!base64Key) {
-  throw new Error('FIREBASE_KEY_BASE64 is not set in environment variables.');
-}
-
-const serviceAccount = JSON.parse(
-  Buffer.from(base64Key, 'base64').toString('utf-8')
-);
+const serviceAccountPath = path.resolve('./src/config/fireBaseConnectKey.json'); 
+const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf-8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'digisky-25d9e.firebasestorage.app', // Replace with your actual bucket
+  storageBucket: 'digisky-25d9e.firebasestorage.app',
 });
 
 const bucket = admin.storage().bucket();
