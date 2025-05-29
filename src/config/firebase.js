355cca2c6bfd8732e22d4c440436d16de0c1a000
bhavya-bansal-29 <bhavyabansal1357@gmail.com>
2025-05-29 12:21:50 +0530

    deployment error

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 import admin from 'firebase-admin';
 import { readFileSync } from 'fs';
 import path from 'path';
-const base64Key = process.env.FIREBASE_KEY_BASE64;
+const base64Key = process.env.FireBase_Key_BASE64;
 
 if (!base64Key) {
   throw new Error('FIREBASE_KEY_BASE64 is not set in environment variables.');
