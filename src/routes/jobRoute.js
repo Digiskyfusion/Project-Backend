3627e1e8be39f17a0fd18370d82a5f62c2d7d4d6
Aman Saini <amansaini@gma.com>
2025-06-11 15:21:30 +0530

    search functionality

--- src/routes/jobRoute.js
+++ src/routes/jobRoute.js
@@ -1,13 +1,14 @@
 import express from "express";
 const router = express.Router();
 import {protect}  from "../middleware/authMiddleware.js"
-import {getAllJobs,postJob,deleteJob,updateJob,getJobById} from "../controller/jobControl.js";
+import {getAllJobs,postJob,deleteJob,updateJob,getJobById,searchJobs} from "../controller/jobControl.js";
 
 router.route("/").get(protect,getAllJobs);
 router.route("/").post(protect,postJob);
 router.route("/:id").delete(protect,deleteJob);
 router.route("/:id").put(protect,updateJob);
 router.get("/:id",protect,getJobById); // 👈 Add this
+router.get("/searchjobs/:keyword",searchJobs);
 // router.route("/api/user/me").put(protect,findById);
 
 export default router;
