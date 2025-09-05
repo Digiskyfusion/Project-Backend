03c200b86911e6192ddcb011f379136738ece3c6
react <reactjs@techquadra.com>
2025-09-05 18:18:39 +0530

    >

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createOrder, verifyPayment ,getUserReceipts, createSubscription,  verifySubscription} from "../controller/paymentController.js";
 
 const router = express.Router();
-
+// 
 router.post("/create-order", createOrder);
 router.post("/verify-payment", verifyPayment);
 router.post('/payment/verify-subscription', verifySubscription);
